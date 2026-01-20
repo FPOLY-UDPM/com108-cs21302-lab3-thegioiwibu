3358b9113d28fd175ce853925bc19f7841c960fc
hoàng anh <thegioiwibu3779@gmail.com>
2026-01-20 09:47:00 +0700

    lap 1

--- bai1.c
+++ bai1.c
@@ -20,12 +20,39 @@
 int main(){
 
     // Khai báo biến
+int main(void) {
+    setlocale(LC_ALL, "");
 
+    float diem;v
 
     // Nhập dữ liệu
-
+printf("Nhập điểm của sinh viên (0–10): ");
+    scanf("%f", &diem);
 
     // Xử lý, tính toán VÀ Hiển thị kết quả
+if (diem < 0 || diem > 10) {
+        printf("Điểm không hợp lệ. Vui lòng nhập từ 0 đến 10.\n");
+    }
+    else if (diem >= 9) {
+        printf("Học lực: Xuất sắc\n");
+    }
+    else if (diem >= 8) {
+        printf("Học lực: Giỏi\n");
+    }
+    else if (diem >= 6.5) {
+        printf("Học lực: Khá\n");
+    }
+    else if (diem >= 5) {
+        printf("Học lực: Trung bình\n");
+    }
+    else if (diem >= 3.5) {
+        printf("Học lực: Yếu\n");
+    }
+    else {
+        printf("Học lực: Kém\n");
+    }
 
+    return 0;
+}
     return 0;
 }
